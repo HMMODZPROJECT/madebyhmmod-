692cf85b213eb0e565a42fa688018b49c27a4ca6
hmmodzvip <hmvip2291@gmail.com>
2025-12-16 16:54:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const channel = {
   name: "𝗛𝗠 𝗠𝗢𝗗𝗭 𝗩𝗜𝗣",
-  handle: "@hmmodzvipreal",desc: "USER IDIOT TOLOL KONTOL LU SEMUA ANJG PAKE DOANG TOLOL.",
+  handle: "@hmmodzvipreal",desc: "𝚠𝚎𝚋𝚜𝚒𝚝𝚎 𝚒𝚗𝚒 𝚍𝚒 𝚔𝚎𝚖𝚋𝚊𝚗𝚐𝚔𝚊𝚗 𝚘𝚕𝚎𝚑 𝙷𝙼 𝙼𝙾𝙳𝚉 𝚃𝙴𝙰𝙼.",
   footer: "HM MODZ TEAM"
 };
 
@@ -20,10 +20,157 @@ function openInBrowser(url) {
 
 const files = [
   {
-    name: "Kontol bapa lu",
-    desc: "bapa lu di ewe monyet anjg",
-    size: "36.8 MB",
-    link: "https://www.youareanidiot.cc/lol.html",
+    name: "FF INJECTOR",
+    desc: "LATEST INJECTOR APK PANEL VIP",
+    size: "23.08 MB",
+    link: "https://www.mediafire.com/file/voyxrwviutiw5bk/PANEL_ANDROID.zip/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "INJECTOR FF NEW OB50",
+    desc: "Dark Aura Injektor cracked",
+    size: "19.7 MB",
+    link: "https://www.mediafire.com/file/kuh441hy3gf0gsr/DARK+AURA#cracked.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Bloodstrike Loader",
+    desc: "Loader, cracked key",
+    size: "29 MB",
+    link: "https://www.mediafire.com/file/4qaeb0onh0q998a/ZR+MODZ+OFFICIAL#cracked.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Harley Cheat Cracked",
+    desc: "Cracked Login Key, Paste You Abstark key",
+    size: "14.28 MB",
+    link: "https://www.mediafire.com/file/ob0fvf1r2hsgvcp/Harleycheat+Loader_4.0#Crackedbyhmmodz.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Anime Play Modded Premium",
+    desc: "Premium Unlocked",
+    size: "17.1 MB",
+    link: "https://www.mediafire.com/file/lgpf4fexwmyzvdk/AnimePlay_1.1.7.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Arena Breakout Mod",
+    desc: "Mod Menu",
+    size: "1.21 MB",
+    link: "https://mega.nz/file/RU8DAJwR#bAI8lz3lPNeNQkZa4HcMmN0Wxaj-EkqxvWuOlOw2094",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "FF Headtrick vip",
+    desc: "Headtrick vip",
+    size: "14.2 MB",
+    link: "https://www.mediafire.com/file/apd5a29nwkiuja4/app_headtrik_albobx%25C2%25A1t%2523CrackedByhmmodz.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "PUBGM LOADER CRACKED",
+    desc: "random key, Esp Line, Aimbot",
+    size: "11.2 MB",
+    link: "https://www.mediafire.com/file/okbkt08xv26zi16/XTHE+KING+LOADER_1.0+#Crack.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "IMMORTAL OFFICIAL CRACKED",
+    desc: "random key, Esp Line, Aimbot",
+    size: "11.2 MB",
+    link: "https://www.mediafire.com/file/8dntuh1ok6zzl4c/AnKus_Panel%2523Cracked_Key.apk/file?dkey=7gcutj0sj72&r=628",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Panel Non Root Cracked",
+    desc: "random key, Esp Line, Aimbot",
+    size: "5,6 MB",
+    link: "https://sfl.gl/VEA6rIv",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Mlbb Cheat CRACKED",
+    desc: "no key, Esp Line, Aimbot",
+    size: "149.2 MB",
+    link: "https://www.mediafire.com/file/00orkxwxzn08sfh/Cracked_Key_mlbb.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+   },
+   {
+    name: "Injector Ob51 Cracked",
+    desc: "Random Key, Aimbot, Hologram vip",
+    size: "11.7 MB",
+    link: "https://t.me/shivam_h4cks_store/185?single",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Bst premium Cracked",
+    desc: "Random Key, Aimbot, esp line, Pubgm 64 Bit",
+    size: "11.45 MB",
+    link: "https://www.mediafire.com/file/k2kxjqtvmv7dubr/best+cheat+-+premium#RandomKeybyhmmodz.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+    {
+    name: "Netflix Premium Modz apk",
+    desc: "Premium Unlocked ",
+    size: "60 MB",
+    link: "https://www.mediafire.com/file/tuoksnwanvjnvdh/Netflix_Mirror_Premium_v1.0_adescargar.online.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Grand theft auto Vice Story",
+    desc: "Unlocked VIP game ",
+    size: "1,5 GB",
+    link: "https://www.mediafire.com/file/zp5mx1p63akgdjh/Grand+Theft+Auto+Vice+City+Stories+v1.0_Adescargar.online.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Shadow Mod Pubgm",
+    desc: "Skip Login",
+    size: "14.77 MB",
+    link: "https://www.mediafire.com/file/oldlrwbfayv7493/SHADOWCHEAT+PREMIUM#hmmodz_str_crack.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Mars Loader Cracked by hmmodzvip",
+    desc: "Skip Login and Random Key",
+    size: "8.31 MB",
+    link: "https://www.mediafire.com/file/bvnybayu3qy44hn/Mars+Loader+Leaks+By+HM+MODZ+VIP.zip/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Movie Box Cracked",
+    desc: "Premium Unlocked",
+    size: "62.86 MB",
+    link: "https://www.mediafire.com/file/6vlo63sdgz2jq66/Movie+Box+Premium.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Melolo Dracin (Asik cuy)",
+    desc: "Premium Unlocked",
+    size: "67.86 MB",
+    link: "https://www.mediafire.com/file/v8jamk7vqttjujj/Melolo+Dracin+Premium.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Panel Root/Non Root FF",
+    desc: "Auto Login",
+    size: "38.86 MB",
+    link: "https://www.mediafire.com/file/qil691dr5nag9gd/Panel+By+HM+MODZ+VIP.zip/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "Grand Theft Auto San Andreas",
+    desc: "Latest Apk",
+    size: "2,6 GB",
+    link: "https://www.mediafire.com/file/ox09gz6p323t4kp/Grand_Theft_Auto_San_Andreas_v2.11.277_ADESCARGAR.online.apk/file",
+    logo: "https://files.catbox.moe/9qs9kl.jpg"
+  },
+  {
+    name: "ROBOT V4.1#Crackedbyhmmodz",
+    desc: "Skip Login Page,pubgm",
+    size: "14,7 MB",
+    link: "https://www.mediafire.com/file/okn2ei5fsmkpkoa/ROBOT+V4.1#Crackedbyhmmodz.apk/file",
     logo: "https://files.catbox.moe/9qs9kl.jpg"
   },
   
@@ -40,7 +187,7 @@ function renderFiles(list) {
       <h3>${f.name}</h3>
       <p>${f.desc}</p>
       <p><strong>SIZE:</strong> ${f.size}</p>
-      <button class="btn">⬇️ DOWNLOAD</button>
+      <button class="btn">⬇️ Unduh</button>
     `;
 
     const btn = card.querySelector(".btn");
